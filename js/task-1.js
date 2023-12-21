8ec246bbb184da874281b71d8775ac6e20540a2b
Medvedua <kaliuzhniy@gmail.com>
2023-12-22 01:29:23 +0300

    modul-3

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,16 @@
 function slugify(title) {
+    const lowerCase = title.toLowerCase();
+    const split = lowerCase.split(" ");
+    const slug = split.join("-");
 
+    return slug;
+    
 }
+
+
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+
--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,17 @@
 function filterArray(numbers, value) {
-
+    const totalNumbers = []
+    for (const number of numbers) {
+        if (number > value) {
+            totalNumbers.push(number);
+        }
+        
+    }
+    return totalNumbers;
 }
+console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+console.log(filterArray([1, 2, 3, 4, 5], 4)); 
+console.log(filterArray([1, 2, 3, 4, 5], 5)); 
+console.log(filterArray([12, 24, 8, 41, 76], 38)); 
+console.log(filterArray([12, 24, 8, 41, 76], 20)); 
+
+
